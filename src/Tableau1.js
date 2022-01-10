7d9e4f7fc8926cb883892f15bd3273e66a0e8a92
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2022-01-10 20:09:06 +0100

    Update Tableau1.js

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -12,15 +12,29 @@ class Tableau1 extends Phaser.Scene{
         this.hauteur=500
         this.largeur=1000
 
+        this.haut = this.physics.add.image(0,0,"carre").setOrigin(0,0)
+        this.haut.setDisplaySize(this.largeur,20)
+        this.haut.body.setAllowGravity(false)
+        this.haut.setImmovable(true)
+
+        this.bas = this.physics.add.image(0,this.hauteur-20,"carre").setOrigin(0,0)
+        this.bas.setDisplaySize(this.largeur,20)
+        this.bas.body.setAllowGravity(false)
+        this.bas.setImmovable(true)
+
         this.gauche = this.physics.add.image(0,this.hauteur/2,"carre").setOrigin(0,0)
         this.gauche.setDisplaySize(20,100)
         this.gauche.body.setAllowGravity(false)
         this.gauche.setImmovable(true)
+        this.physics.add.collider(this.gauche,this.haut)
+        this.physics.add.collider(this.gauche,this.bas)
 
         this.droite = this.physics.add.image(this.largeur-20,this.hauteur/2,"carre").setOrigin(0,0)
         this.droite.setDisplaySize(20,100)
         this.droite.body.setAllowGravity(false)
         this.droite.setImmovable(true)
+        this.physics.add.collider(this.droite,this.haut)
+        this.physics.add.collider(this.droite,this.bas)
 
         this.balle = this.physics.add.image(this.largeur/2,this.hauteur/2,"cercle").setOrigin(0,0)
         this.balle.setDisplaySize(20,20)
@@ -28,6 +42,10 @@ class Tableau1 extends Phaser.Scene{
         this.balle.setVelocityX(100)
         this.balle.setVelocityY(100)
         this.balle.body.setMaxVelocityY(500)
+        this.physics.add.collider(this.balle,this.haut)
+        this.physics.add.collider(this.balle,this.bas)
+        this.physics.add.collider(this.balle,this.gauche)
+        this.physics.add.collider(this.balle,this.droite)
 
         this.touches()
     }
@@ -42,20 +60,43 @@ class Tableau1 extends Phaser.Scene{
         {
             switch (kevent.keyCode)
             {
-                case Phaser.Input.Keyboard.KeyCodes.A:
+                case Phaser.Input.Keyboard.KeyCodes.S:
                     me.vitesseGauche = 1
                     console.log(me.vitesseGauche)
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.Q:
+                case Phaser.Input.Keyboard.KeyCodes.X:
                     me.vitesseGauche = -1
                     console.log(me.vitesseGauche)
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.P:
+                case Phaser.Input.Keyboard.KeyCodes.J:
                     me.vitesseDroite = 1
                     console.log(me.vitesseDroite)
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.M:
+                case Phaser.Input.Keyboard.KeyCodes.N:
                     me.vitesseDroite = -1
+                    console.log(me.vitesseDroite)
+                    break;
+            }
+        });
+        //touches relachées
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    me.vitesseGauche = 0
+                    console.log(me.vitesseGauche)
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.X:
+                    me.vitesseGauche = 0
+                    console.log(me.vitesseGauche)
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                    me.vitesseDroite = 0
+                    console.log(me.vitesseDroite)
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.N:
+                    me.vitesseDroite = 0
                     console.log(me.vitesseDroite)
                     break;
             }
@@ -65,9 +106,7 @@ class Tableau1 extends Phaser.Scene{
     update()
     {
 
-        this.vitesseDroite = 0;
-        this.vitesseGauche = 0;
-
+        /*
         if(this.balle.y >= this.hauteur)
         {
             this.balle.y = 0
@@ -81,36 +120,26 @@ class Tableau1 extends Phaser.Scene{
         {
             this.balle.x = 0
         }
+         */
 
         //déplacement des raquettes
-        if(this.vitesseGauche = 1)
+        if(this.vitesseGauche === 1)
         {
-            if(this.gauche.x >= 50)
-            {
-                this.gauche.setPosition(this.gauche.x,this.gauche.y-10)
-            }
+            this.gauche.setPosition(this.gauche.x,this.gauche.y-1)
         }
-        if(this.vitesseGauche = -1)
+        if(this.vitesseGauche === -1)
         {
-            if(this.gauche.x <= this.hauteur)
-            {
-                this.gauche.setPosition(this.gauche.x,this.gauche.y+10)
-            }
+            this.gauche.setPosition(this.gauche.x,this.gauche.y+1)
         }
 
-        if(this.vitesseDroite = 1)
+        if(this.vitesseDroite === 1)
         {
-            if(this.droite.x >= 50)
-            {
-                this.droite.setPosition(this.droite.x,this.droite.y-10)
-            }
+            this.droite.setPosition(this.droite.x,this.droite.y-1)
         }
-        if(this.vitesseDroite = -1)
+        if(this.vitesseDroite === -1)
         {
-            if(this.droite.x <= this.droite)
-            {
-                this.droite.setPosition(this.droite.x,this.droite.y+10)
-            }
+            this.droite.setPosition(this.droite.x,this.droite.y+1)
+
         }
     }
 }
