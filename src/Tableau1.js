62174ccb725f594feea7ff4246fa8d41d4793e94
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2022-01-17 15:56:48 +0100

    1

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -8,44 +8,41 @@ class Tableau1 extends Phaser.Scene{
 
     create()
     {
+        let me = this;
 
         this.hauteur=500
         this.largeur=1000
 
-        this.haut = this.physics.add.image(0,0,"carre").setOrigin(0,0)
+        this.haut = this.physics.add.image(0,-20,"carre").setOrigin(0,0)
         this.haut.setDisplaySize(this.largeur,20)
         this.haut.body.setAllowGravity(false)
         this.haut.setImmovable(true)
 
-        this.bas = this.physics.add.image(0,this.hauteur-20,"carre").setOrigin(0,0)
+        this.bas = this.physics.add.image(0,this.hauteur,"carre").setOrigin(0,0)
         this.bas.setDisplaySize(this.largeur,20)
         this.bas.body.setAllowGravity(false)
         this.bas.setImmovable(true)
 
-        this.gauche = this.physics.add.image(0,this.hauteur/2,"carre").setOrigin(0,0)
+        this.gauche = this.physics.add.image(30,this.hauteur/2-50,"carre").setOrigin(0,0)
         this.gauche.setDisplaySize(20,100)
         this.gauche.body.setAllowGravity(false)
         this.gauche.setImmovable(true)
-        this.physics.add.collider(this.gauche,this.haut)
-        this.physics.add.collider(this.gauche,this.bas)
 
-        this.droite = this.physics.add.image(this.largeur-20,this.hauteur/2,"carre").setOrigin(0,0)
+        this.droite = this.physics.add.image(this.largeur-50,this.hauteur/2-50,"carre").setOrigin(0,0)
         this.droite.setDisplaySize(20,100)
         this.droite.body.setAllowGravity(false)
         this.droite.setImmovable(true)
-        this.physics.add.collider(this.droite,this.haut)
-        this.physics.add.collider(this.droite,this.bas)
 
         this.balle = this.physics.add.image(this.largeur/2,this.hauteur/2,"cercle").setOrigin(0,0)
         this.balle.setDisplaySize(20,20)
         this.balle.setBounce(1.5,1.5)
         this.balle.setVelocityX(100)
         this.balle.setVelocityY(100)
-        this.balle.body.setMaxVelocityY(300)
+        this.balle.body.setMaxVelocity(300)
         this.physics.add.collider(this.balle,this.haut)
         this.physics.add.collider(this.balle,this.bas)
-        this.physics.add.collider(this.balle,this.gauche)
-        this.physics.add.collider(this.balle,this.droite)
+        this.physics.add.collider(this.balle,this.gauche,function(){me.rebond(me.gauche)});
+        this.physics.add.collider(this.balle,this.droite,function(){me.rebond(me.droite)});
 
         this.touches()
     }
@@ -62,19 +59,15 @@ class Tableau1 extends Phaser.Scene{
             {
                 case Phaser.Input.Keyboard.KeyCodes.S:
                     me.vitesseGauche = 1
-                    console.log(me.vitesseGauche)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.X:
                     me.vitesseGauche = -1
-                    console.log(me.vitesseGauche)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.J:
                     me.vitesseDroite = 1
-                    console.log(me.vitesseDroite)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.N:
                     me.vitesseDroite = -1
-                    console.log(me.vitesseDroite)
                     break;
             }
         });
@@ -85,71 +78,72 @@ class Tableau1 extends Phaser.Scene{
             {
                 case Phaser.Input.Keyboard.KeyCodes.S:
                     me.vitesseGauche = 0
-                    console.log(me.vitesseGauche)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.X:
                     me.vitesseGauche = 0
-                    console.log(me.vitesseGauche)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.J:
                     me.vitesseDroite = 0
-                    console.log(me.vitesseDroite)
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.N:
                     me.vitesseDroite = 0
-                    console.log(me.vitesseDroite)
                     break;
             }
         });
     }
 
+    rebond(raquette)
+    {
+        let hauteurRaquette = raquette.displayHeight;
+
+        let positionRelativeRaquette = (this.balle.y - raquette.y);
+
+        positionRelativeRaquette = (positionRelativeRaquette / hauteurRaquette)
+        positionRelativeRaquette = positionRelativeRaquette*2-1;
+
+        this.balle.setVelocityY(this.balle.body.velocity.y + positionRelativeRaquette * 1000);
+    }
+
+    out()
+    {
+        this.balle.x = this.largeur/2
+        this.balle.y = this.hauteur/2
+        this.balle.setVelocityX(100)
+        this.balle.setVelocityY(100)
+    }
+
+
     update()
     {
 
-        /*
-        if(this.balle.y >= this.hauteur)
-        {
-            this.balle.y = 0
-        }
-
-        if(this.balle.x > this.largeur)
-        {
-            this.balle.x = this.largeur
-        }
-        if(this.balle.x < 0)
-        {
-            this.balle.x = 0
-        }
-         */
-
         //déplacement des raquettes
-        if(this.vitesseGauche === 1 && this.gauche.y>20)
+        if(this.vitesseGauche === 1 && this.gauche.y>0)
         {
             this.gauche.setPosition(this.gauche.x,this.gauche.y-5)
         }
-        if(this.vitesseGauche === -1 && this.gauche.y<this.hauteur-120)
+        if(this.vitesseGauche === -1 && this.gauche.y<this.hauteur-100)
         {
-            console.log(this.gauche.y)
             this.gauche.setPosition(this.gauche.x,this.gauche.y+5)
         }
 
-        if(this.vitesseDroite === 1 && this.droite.y>20)
+        if(this.vitesseDroite === 1 && this.droite.y>0)
         {
             this.droite.setPosition(this.droite.x,this.droite.y-5)
         }
-        if(this.vitesseDroite === -1 && this.droite.y<this.hauteur-120)
+        if(this.vitesseDroite === -1 && this.droite.y<this.hauteur-100)
         {
             this.droite.setPosition(this.droite.x,this.droite.y+5)
 
         }
 
         //sortie de balle
-        if (this.balle.x <=0 || this.balle.x>=this.largeur)
+        if (this.balle.x>=this.largeur)
         {
-            this.balle.x = this.largeur/2
-            this.balle.y = this.hauteur/2
-            this.balle.setVelocityX(100)
-            this.balle.setVelocityY(100)
+            this.out()
+        }
+        if (this.balle.x <=0)
+        {
+            this.out()
         }
     }
 }
